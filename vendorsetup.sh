d0d1f63a3db08da21166c9dc9f66345b1bdaeef1
Sergey Larin <cerg2010cerg2010@mail.ru>
2019-10-26 21:36:43 +0000

    Fix patch directory
    Change-Id: I4eeb61d5d048f630fcf6d1bc4510037422431d20

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -57,7 +57,7 @@ echo -n "Apply patch 0001-external-skia-patch.patch"
 if [ $? == 0 ]; then
 	echo "     [DONE]"
 else
-	(cd frameworks/av; git am --abort)
+	(cd external/skia; git am --abort)
 	echo "     [FAIL]"
 fi
 
