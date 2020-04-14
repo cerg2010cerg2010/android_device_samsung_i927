57b3e426b35724f92be576c6370b16044838f78e
Sergey Larin <cerg2010cerg2010@mail.ru>
2020-04-14 22:14:37 +0000

    Add more patches, rework patch script
    Patches are:
     - one is the minor ART fix, detected when running with libartd.so.
     - TLSv1.1 & TLSv1.2 are now enabled by default - should fix Magisk
       updates and Slack.
     - And the NPE fix in the image editor... Shame!

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -24,123 +24,29 @@
 #add_lunch_combo full_i927-eng
 add_lunch_combo cm_i927-userdebug
 
-echo "Apply patch to frameworks/base"
-echo -n "Apply patch 0001-framework-base-patch.patch"
-(cd frameworks/base; git am ../../device/samsung/i927/patches/0001-framework-base-patch.patch) > /dev/null 2>&1
-if [ $? == 0 ]; then
-	echo "     [DONE]"
-else
-	(cd frameworks/base; git am --abort)
-	echo "     [FAIL]"
-fi
+function apply_patch()
+{
+	printf "%s" "Apply patch to $1 - $(basename $2)"
+	(cd $1; git am $2) > /dev/null 2>&1
+	if [ $? == 0 ]; then
+		echo "     [DONE]"
+	else
+		(cd $1; git am --abort)
+		echo "     [FAIL]"
+	fi
+}
 
-echo -n "Apply patch 0002-DisplayDevice-Backwards-compatibility-with-old-EGL.patch"
-(cd frameworks/native; git am ../../device/samsung/i927/patches/0002-DisplayDevice-Backwards-compatibility-with-old-EGL.patch) > /dev/null 2>&1
-if [ $? == 0 ]; then
-	echo "     [DONE]"
-else
-	(cd frameworks/native; git am --abort)
-	echo "     [FAIL]"
-fi
-echo -n "Apply patch 0002-Add-missing-functions-and-signatures-for-older-OMX-v.patch"
-(cd frameworks/av; git am ../../device/samsung/i927/patches/0002-Add-missing-functions-and-signatures-for-older-OMX-v.patch) > /dev/null 2>&1
-if [ $? == 0 ]; then
-	echo "     [DONE]"
-else
-	(cd frameworks/av; git am --abort)
-	echo "     [FAIL]"
-fi
-
-
-echo -n "Apply patch 0001-external-skia-patch.patch"
-(cd external/skia; git am ../../device/samsung/i927/patches/0001-external-skia-patch.patch) > /dev/null 2>&1
-if [ $? == 0 ]; then
-	echo "     [DONE]"
-else
-	(cd external/skia; git am --abort)
-	echo "     [FAIL]"
-fi
-
-echo "Apply patch to external/chromium_org"
-echo -n "Apply patch 0001-Work-around-broken-GL_TEXTURE_BINDING_EXTERNAL_OES-q.patch"
-(cd external/chromium_org; git am ../../device/samsung/i927/patches/0001-Work-around-broken-GL_TEXTURE_BINDING_EXTERNAL_OES-q.patch) > /dev/null 2>&1
-if [ $? == 0 ]; then
-  echo "     [DONE]"
-else
-  (cd external/chromium_org; git am --abort)
-  echo "     [FAIL]"
-fi
-
-echo "Apply patch to frameworks/native"
-echo -n "Apply patch 0001-Fix-layer-dump-for-tegra2.patch"
-(cd frameworks/native; git am ../../device/samsung/i927/patches/0001-Fix-layer-dump-for-tegra2.patch) > /dev/null 2>&1
-if [ $? == 0 ]; then
-       echo "     [DONE]"
-else
-       (cd frameworks/native; git am --abort)
-       echo "     [FAIL]"
-fi
-
-
-echo "Apply patch to hardware/samsung"
-echo -n "Apply patch 0001-rild-uses-tcp-socket.patch"
-(cd hardware/samsung; git am ../../device/samsung/i927/patches/0001-rild-uses-tcp-socket.patch) > /dev/null 2>&1
-if [ $? == 0 ]; then
-       echo "     [DONE]"
-else
-       (cd hardware/samsung; git am --abort)
-       echo "     [FAIL]"
-fi
-
-
-
-echo "Apply patch to frameworks/base"
-echo -n "Apply patch legacy_startvm.patch"
-(cd frameworks/base; git am ../../device/samsung/i927/patches/legacy_startvm.patch) > /dev/null 2>&1
-if [ $? == 0 ]; then
-    echo "     [DONE]"
-else
-    (cd frameworks/base; git am --abort)
-    echo "     [FAIL]"
-fi
-
-
-echo "Apply patch to bionic"
-echo -n "Apply patch 0003-Add-tegra2-to-bionic.patch"
-(cd bionic; git am ../device/samsung/i927/patches/0003-Add-tegra2-to-bionic.patch) > /dev/null 2>&1
-if [ $? == 0 ]; then
-    echo "     [DONE]"
-else
-    (cd bionic; git am --abort)
-    echo "     [FAIL]"
-fi
-
-echo "Apply patch to art"
-echo -n "Apply patch 0001-fix-art-crashes.patch"
-(cd art; git am ../device/samsung/i927/patches/0001-fix-art-crashes.patch) > /dev/null 2>&1
-if [ $? == 0 ]; then
-    echo "     [DONE]"
-else
-    (cd art; git am --abort)
-    echo "     [FAIL]"
-fi
-
-echo "Apply patch to external/bluetooth/bluedroid"
-echo -n "Apply patch 0001-Increase-stream-buffer.patch"
-(cd external/bluetooth/bluedroid; git am ../../../device/samsung/i927/patches/0001-Increase-stream-buffer.patch) > /dev/null 2>&1
-if [ $? == 0 ]; then
-    echo "     [DONE]"
-else
-    (cd external/bluetooth/bluedroid; git am --abort)
-    echo "     [FAIL]"
-fi
-
-echo "Apply patch to frameworks/base"
-echo -n "Apply patch 0001-Signature-spoofing.patch"
-(cd frameworks/base; git am ../../device/samsung/i927/patches/0001-Signature-spoofing.patch) > /dev/null 2>&1
-if [ $? == 0 ]; then
-    echo "     [DONE]"
-else
-    (cd frameworks/base; git am --abort)
-    echo "     [FAIL]"
-fi
+apply_patch "frameworks/base" "../../device/samsung/i927/patches/0001-framework-base-patch.patch"
+apply_patch "frameworks/native" "../../device/samsung/i927/patches/0002-DisplayDevice-Backwards-compatibility-with-old-EGL.patch"
+apply_patch "frameworks/av" "../../device/samsung/i927/patches/0002-Add-missing-functions-and-signatures-for-older-OMX-v.patch"
+apply_patch "external/skia" "../../device/samsung/i927/patches/0001-external-skia-patch.patch"
+apply_patch "external/chromium_org" "../../device/samsung/i927/patches/0001-Work-around-broken-GL_TEXTURE_BINDING_EXTERNAL_OES-q.patch"
+apply_patch "frameworks/native" "../../device/samsung/i927/patches/0001-Fix-layer-dump-for-tegra2.patch"
+apply_patch "hardware/samsung" "../../device/samsung/i927/patches/0001-rild-uses-tcp-socket.patch"
+apply_patch "frameworks/base" "../../device/samsung/i927/patches/legacy_startvm.patch"
+apply_patch "bionic" "../device/samsung/i927/patches/0003-Add-tegra2-to-bionic.patch"
+apply_patch "art" "../device/samsung/i927/patches/0001-fix-art-crashes.patch"
+apply_patch "external/bluetooth/bluedroid" "../../../device/samsung/i927/patches/0001-Increase-stream-buffer.patch"
+apply_patch "frameworks/base" "../../device/samsung/i927/patches/0001-Signature-spoofing.patch"
+apply_patch "packages/apps/Gallery2" "../../../device/samsung/i927/patches/0001-Fix-crash-in-image-editor.patch"
+apply_patch "libcore" "../device/samsung/i927/patches/0001-Enable-TLSv1.1-and-TLSv1.2-by-default.patch"
